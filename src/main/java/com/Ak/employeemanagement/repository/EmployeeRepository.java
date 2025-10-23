c8296f03fa7c06b8cc0c2fcaa019c5e96e697eca
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2025-10-23 12:29:10 +0530

    Add EmployeeRepository for CRUD operations

---
+++ src/main/java/com/Ak/employeemanagement/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.Ak.employeemanagement.repository;
+
+import com.Ak.employeemanagement.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+}
