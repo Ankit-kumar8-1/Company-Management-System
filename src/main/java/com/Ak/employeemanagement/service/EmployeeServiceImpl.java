bf9b9033f4d9f0e117b8b6abcaf1db02431b32b0
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2025-10-24 15:39:05 +0530

    Implement business logic in EmployeeService and EmployeeServiceImpl

--- src/main/java/com/Ak/employeemanagement/service/EmployeeServiceImpl.java
+++ src/main/java/com/Ak/employeemanagement/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.Ak.employeemanagement.service;
 
 
 import com.Ak.employeemanagement.model.Employee;
+import com.Ak.employeemanagement.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,35 +11,35 @@ import java.util.Optional;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private EmployeeService employeeService;
+    private EmployeeRepository employeeRepository;
 
-    EmployeeServiceImpl(EmployeeService employeeService){
-        this.employeeService=employeeService;
+    EmployeeServiceImpl(EmployeeRepository employeeRepository){
+        this.employeeRepository=employeeRepository;
     }
 
 
     @Override
     public Employee saveEmployee(Employee employee) {
-        return null;
+        return employeeRepository.save(employee);
     }
 
     @Override
     public List<Employee> getAllEmployee() {
-        return List.of();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Optional<Employee> getEmployeeById(Long id) {
-        return Optional.empty();
+        return employeeRepository.findById(id);
     }
 
     @Override
     public Employee updateEmployee(Employee employee) {
-        return null;
+        return employeeRepository.save(employee);
     }
 
     @Override
     public void deleteEmployee(Long id) {
-
+        employeeRepository.deleteById(id);
     }
 }
