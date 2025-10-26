ada56fdbac751c580527f4027e8c6581284e0783
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2025-10-26 11:20:26 +0530

    Fix: Add GlobalExceptionHandler to handle unsupported HTTP methods (GET/PUT only) Fix: → indicate kar raha hai ki ye bug/issue fix kar raha hai

--- src/main/java/com/Ak/CompanyManagement/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/Ak/CompanyManagement/Exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.Ak.CompanyManagement.Exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +12,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InValidUpdateFieldException.class)
     public ResponseEntity<String> handlerInvalidException(InValidUpdateFieldException ex){
         return  new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<String> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+        return new ResponseEntity<>("You can only perform GET or PUT method.", HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
